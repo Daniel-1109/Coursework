c53d9fde4e22d42bc5660452c63fa410cc25b903
Daniel-1109 <84267743+Daniel-1109@users.noreply.github.com>
2021-06-10 22:47:01 +0630

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,14 @@
+let price = 20;
+switch  (price) {
+    case    price < 20:
+        console.log("price is too low");
+        break;
+    case    price > 20:
+        console.log("price is too big");
+        break;
+    case    price = 20:
+        console.log("perfect price!!");
+        break;
+    default:
+        console.log("This is not a valid price");
+}
