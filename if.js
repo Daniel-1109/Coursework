83a5d46797e91d8b9669630540f28ec7b9898ddc
Daniel-1109 <84267743+Daniel-1109@users.noreply.github.com>
2021-06-10 22:56:36 +0630

    Add files via upload

---
+++ if.js
@@ -0,0 +1,15 @@
+let x = "Mobile Legend";
+let y = "is a mobile game";
+let z = "is not a mobile game";
+if  (x == "Dota 2")  {
+    console.log("Dota 2 " + z);
+}
+else if (x == "Apex Legend")    {
+    console.log("Apex Legend " + z);
+}
+else if (x == "Mobile Legend")  {
+    console.log("Mobile Legend " + y);
+}
+else    {
+    console.log("Undefined Game");
+}
